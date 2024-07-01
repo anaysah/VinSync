85b6277b2eb5a316c681946f1202f6fe761417f0
anaysah <ajaysahu0606@gmail.com>
2024-07-01 21:28:50 +0530

    chore: basic setup for Chrome extension
    - Created necessary folders and files
    - Added manifest.json
    - Set up basic HTML, CSS, and JavaScript files
    - added tailwindcss

---
+++ extension/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      width: {
+        300: "300px",
+      },
+      height: {
+        400: "400px",
+      },
+    },
+  },
+  plugins: [],
+};
